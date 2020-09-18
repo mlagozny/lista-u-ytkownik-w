8fb715f9c9a9afac6ca2bb180684731678ea614d
mlagozny <mateusz_lagozny@o2.pl>
2020-09-18 09:18:29 +0200

    users list

---
+++ src/Users.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react"
+import './Users.css'
+import UsersList from './UsersList'
+
+class Users extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: []
+        }
+    }
+
+
+    addUser = (event) => {
+        event.preventDefault();
+
+
+        let newUser = {
+            id: Date.now(),
+            name: this._inputName.value
+        }
+
+        this.setState((state) => {
+            return ({
+                users: state.users.concat(newUser)
+            });
+        })
+
+        console.log(newUser);
+
+        this._inputName.value = '';
+    }
+
+    removeUser = (userID) => {
+
+        this.setState(state => {
+            return({
+                users: state.users.filter((user) => {return(user.id !== userID)})
+            });
+        });
+    
+    }
+
+    render() {
+        return (
+            <div className='users-main'>
+                <h1>User's List</h1>
+                <form onSubmit={this.addUser}>
+                    <input ref={(element) => { this._inputName = element; }} type='text' placeholder='Enter name' />
+                    <button type='submit'>Add user</button>
+                    <UsersList usersList={this.state.users} removeUserMethod = {this.removeUser} />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Users
